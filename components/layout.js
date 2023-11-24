eb013576d12bff918e6917f21436af933cbcc63f
DK Kim <dkim693@wisc.edu>
2023-11-24 17:12:11 -0600

    changed website name

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Dongku Kim';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Dongku\'s Blog';
 
 export default function Layout({ children, home }) {
   return (
